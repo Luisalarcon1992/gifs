2b01efd848bef1ebc5592bfe49a4180101c2be0a
Luis <luisalarcon1992@gmail.com>
2024-02-25 15:02:11 -0300

    terminado

--- src/app/gifs/components/card/card.component.ts
+++ src/app/gifs/components/card/card.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IGif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card',
   templateUrl: './card.component.html',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+    if( !this.gif ) throw new Error('Method not implemented.');
+  }
+
+  @Input()
+  public gif!: IGif;
+
+
 
 }
